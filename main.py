1f3fc02382a709d32fcd81db91e395389f86625a
Apolodjota <blackcloud_2004@hotmail.com>
2025-01-17 06:13:36 -0500

    Diferent logic

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ from tkinter import messagebox
 
 # Configuración inicial
 WIDTH, HEIGHT = 1200, 800
+GRID_SIZE = 30
+HEIGHT = (HEIGHT // GRID_SIZE) * GRID_SIZE
+
 BACKGROUND_COLOR = (0, 0, 0)
 BACTERIA_COLOR = (0, 255, 0)
 TRACE_COLOR = (255, 255, 0)
 TRACE_OVERLAP_COLOR = (255, 0, 0)
 FOOD_COLOR = (255, 0, 255)
+
 FOOD_RADIUS = 5
 BACTERIA_RADIUS = 5
 COLLISION_DISTANCE = FOOD_RADIUS + BACTERIA_RADIUS
 FPS = 1
-GRID_SIZE = 30
 
 # Inicialización de Pygame
 pygame.init()
@@ -23,26 +26,21 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Simulación de Bacteria")
 clock = pygame.time.Clock()
 
-def simple_random_walk():
-    orientacion = random.choice([1,-1])
-    direccion = random.choice([1, -1])
-    if orientacion == 1:
-        return "top" if direccion == 1 else "bottom"
-    else:
-        return "left" if direccion == 1 else "right"
-
 def generate_bacteria_start():
     # Aparece aleatoriamente en una orilla alineada con la cuadrícula
     side = random.choice(["top", "bottom", "left", "right"])
-    # side = simple_random_walk()
+
+    vertical = random.randint(0, (WIDTH) // GRID_SIZE)
+    horizontal = random.randint(0, (HEIGHT) // GRID_SIZE)
+
     if side == "top":
-        return random.randint(0, (WIDTH - 1) // GRID_SIZE) * GRID_SIZE, 0
+        return vertical * GRID_SIZE, 0
     elif side == "bottom":
-        return random.randint(0, (WIDTH - 1) // GRID_SIZE) * GRID_SIZE, HEIGHT - GRID_SIZE
+        return vertical * GRID_SIZE, HEIGHT
     elif side == "left":
-        return 0, random.randint(0, (HEIGHT - 1) // GRID_SIZE) * GRID_SIZE
-    else:  # right
-        return WIDTH - GRID_SIZE, random.randint(0, (HEIGHT - 1) // GRID_SIZE) * GRID_SIZE
+        return 0, horizontal * GRID_SIZE
+    else:
+        return WIDTH, horizontal * GRID_SIZE
 
 def generate_food(num_food):
     return [(random.randint(0, (WIDTH - 1) // GRID_SIZE) * GRID_SIZE,
@@ -80,6 +78,7 @@ def solicitar_datos():
     root = tk.Tk()
     root.title("Configuración inicial")
     root.geometry("280x90")  # Establecer tamaño específico
+    root.eval('tk::PlaceWindow . center')
     root.resizable(False, False)  # Deshabilitar minimizar y maximizar
 
     tk.Label(root, text="Número de ciclos:").grid(row=0, column=0)
@@ -99,9 +98,16 @@ def solicitar_datos():
 
     root.mainloop()
 
+def walk():
+    orientacion = random.choice([1, -1])
+    direccion = random.choice([1, -1])
+    if orientacion == 1:
+        return "up" if direccion == 1 else "down"
+    else:
+        return "right" if direccion == 1 else "left"
+
 def main():
-    # Solicitar datos
-    solicitar_datos()
+    solicitar_datos() # num_cycles, initial_life, food_energy, num_food
     debug = True
 
     # Generar comida
@@ -111,9 +117,8 @@ def main():
     for cycle in range(num_cycles):
         # Inicializar bacteria
         bacteria_position = generate_bacteria_start()
-        bacteria_life = initial_life
         trace = {}
-        moved_steps = -1  # Inicializar en -1 para no contar el punto de aparición
+        moved_steps = 0  # Inicializar en -1 para no contar el punto de aparición
 
         while moved_steps < initial_life:
             for event in pygame.event.get():
@@ -123,10 +128,8 @@ def main():
 
             # Dibujar fondo
             screen.fill(BACKGROUND_COLOR)
-
             # Dibujar cuadrícula
             draw_grid()
-
             # Dibujar comida
             for food_position in food_positions:
                 pygame.draw.circle(screen, FOOD_COLOR, food_position, FOOD_RADIUS)
@@ -139,22 +142,39 @@ def main():
 
             # Movimiento de la bacteria
             x, y = bacteria_position
+            move = None
+            # verificar si se encuentra en una pared
+            if x == 0 or x == WIDTH or y == 0 or y == HEIGHT:
+                # verificar si se encuentra en una esquina
+                if (x, y) in [(0, 0), (WIDTH, 0), (0, HEIGHT), (WIDTH, HEIGHT)]:
+                    while move not in ["left", "up", "right", "down"]:
+                        move = walk()
+                        if (x == 0 and y == 0 and move in ["left", "up"]) or \
+                           (x == WIDTH and y == 0 and move in ["right", "up"]) or \
+                           (x == 0 and y == HEIGHT and move in ["left", "down"]) or \
+                           (x == WIDTH and y == HEIGHT and move in ["right", "down"]):
+                            break
+                else:
+                    while move not in ["left", "right", "up", "down"]:
+                        move = walk()
+                        if (x == 0 and move == "left") or \
+                           (x == WIDTH and move == "right") or \
+                           (y == 0 and move == "up") or \
+                           (y == HEIGHT and move == "down"):
+                            break
+            else:
+                move = walk()
 
-            if moved_steps == -1:  # Primera jugada (sin retroceder)
-                moves = [(0, GRID_SIZE), (GRID_SIZE, 0), (0, -GRID_SIZE), (-GRID_SIZE, 0)]
-                if x == 0:  # Está en la izquierda
-                    moves = [(0, GRID_SIZE), (GRID_SIZE, 0), (0, -GRID_SIZE)]
-                elif x == WIDTH - GRID_SIZE:  # Está en la derecha
-                    moves = [(0, GRID_SIZE), (0, -GRID_SIZE), (-GRID_SIZE, 0)]
-                elif y == 0:  # Está arriba
-                    moves = [(GRID_SIZE, 0), (0, GRID_SIZE), (-GRID_SIZE, 0)]
-                elif y == HEIGHT - GRID_SIZE:  # Está abajo
-                    moves = [(GRID_SIZE, 0), (0, GRID_SIZE), (-GRID_SIZE, 0)]
-            else:  # Movimientos normales
-                moves = [(0, GRID_SIZE), (GRID_SIZE, 0), (0, -GRID_SIZE), (-GRID_SIZE, 0)]
+            if move == "up":
+                y -= GRID_SIZE
+            elif move == "down":
+                y += GRID_SIZE
+            elif move == "right":
+                x += GRID_SIZE
+            else:
+                x -= GRID_SIZE
 
-            dx, dy = random.choice(moves)
-            new_position = (x + dx, y + dy)
+            new_position = (x, y)
 
             if is_inside_screen(*new_position):
                 bacteria_position = new_position
